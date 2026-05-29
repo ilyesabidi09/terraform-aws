179c261f1e6bbbe9a5db4bda607a1725acba0eb2
ilyes abidi <ilyes.abidi.cr@gmail.com>
2026-05-29 23:06:48 +0200

    fix: add SSH ingress rule to DB security group

--- modules/ec2-db/main.tf
+++ modules/ec2-db/main.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "db" {
     cidr_blocks = ["10.0.0.0/16"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
